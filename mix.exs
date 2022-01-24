629f8af6f18fa3a8391d7f9790d4d2b1b11f8bbe
Alan Vardy <alan@alanvardy.com>
2022-01-23 17:37:28 -0800

    wip

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule AlanVardy.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+      {:tailwind, "~> 0.1", only: :dev}
     ]
   end
 
