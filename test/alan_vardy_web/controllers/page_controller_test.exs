e3ccf2f0dd60ee228b16d6fe0fe73a62944af647
Alan Vardy <alan@alanvardy.com>
2022-02-21 11:29:48 -0800

    additional improvements

--- test/alan_vardy_web/controllers/page_controller_test.exs
+++ test/alan_vardy_web/controllers/page_controller_test.exs
@@ -5,4 +5,9 @@ defmodule AlanVardyWeb.PageControllerTest do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "My name is Alan Vardy, I am a Senior Developer"
   end
+
+  test "GET About Me", %{conn: conn} do
+    conn = get(conn, Routes.page_path(conn, :aboutme))
+    assert html_response(conn, 200) =~ "This is the part where I get to talk about myself!"
+  end
 end
