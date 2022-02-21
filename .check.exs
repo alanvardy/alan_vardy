fe756d062255152eed9f0dcb960a4dc32c264936
Alan Vardy <alan@alanvardy.com>
2022-02-21 15:56:00 -0800

    add mix check and tool versions

---
+++ .check.exs
@@ -0,0 +1,12 @@
+[
+  retry: false,
+  tools: [
+    {:formatter, command: "mix format"},
+    {:ex_coveralls,
+     command: "mix coveralls.html",
+     require_files: ["test/test_helper.exs"],
+     env: %{"MIX_ENV" => "test"}},
+    {:credo, command: "mix credo --strict"},
+    {:ex_unit, command: "mix test", env: %{"MIX_ENV" => "test"}}
+  ]
+]
