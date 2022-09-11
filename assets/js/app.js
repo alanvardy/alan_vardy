1349aba31265f19509377e14b688653d676f3a2b
Alan Vardy <alan@vardy.cc>
2022-09-11 10:55:36 -0700

    Add copy buttons to codeblocks

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,17 +21,47 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", info => topbar.show())
 window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+
+// Copy button start
+
+const BUTTON = `<button class="copy-button"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="currentColor"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"/></svg></button>`
+
+export const qsAll = document.querySelectorAll.bind(document)
+
+/**
+ * Find pre tags, add copy buttons, copy <code> content on click.
+ */
+function addCopyButtons() {
+  console.log("started")
+  Array.from(qsAll('pre')).forEach(pre => pre.insertAdjacentHTML('afterbegin', BUTTON))
+
+  Array.from(qsAll('.copy-button')).forEach(button => {
+    console.log("button")
+    let timeout
+    button.addEventListener('click', () => {
+      timeout && clearTimeout(timeout)
+      navigator.clipboard.writeText(button.parentElement.querySelector('code').textContent)
+      button.classList.add('clicked')
+      timeout = setTimeout(() => button.classList.remove('clicked'), 3000)
+    })
+  })
+}
+
+addCopyButtons()
+
+// Copy button end
+
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
