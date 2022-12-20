6229e2516cd68a15344966f6297d8c1ef09ab7ba
Alan Vardy <alan@vardy.cc>
2022-12-20 10:02:58 -0800

    Update Cypress specs

--- cypress/e2e/not_logged_in.cy.js
+++ cypress/e2e/not_logged_in.cy.js
@@ -1,38 +1,39 @@
 describe('Not logged in', function () {
-  beforeEach(function () {
-    // before each test, we can automatically preserve the
-    // 'session_id' and 'remember_token' cookies. this means they
-    // will not be cleared before the NEXT test starts.
-    //
-    // the name of your cookies will likely be different
-    // this is just a simple example
-    Cypress.Cookies.debug(true)
-    Cypress.Cookies.preserveOnce('_alanvardy_key')
-  })
+
 
   it('Visits the main page', function () {
     cy.visit('http://localhost:4000')
     cy.contains('My name is Alan Vardy, I am a Senior Developer')
+    
   })
-
+  
   it('Visits the blog page', function () {
+    cy.visit('http://localhost:4000')
     cy.get('[href="/blog"]').first().click();
     cy.contains('Previous')
     cy.contains('Next')
+    
   })
-
+  
   it('Clicks on an elixir tag', function () {
+    cy.visit('http://localhost:4000')
+    cy.get('[href="/blog"]').first().click();
+    
     cy.contains('elixir').click();
     cy.contains('#elixir')
   })
-
+  
   it('Visits the about me page', function () {
+    cy.visit('http://localhost:4000')
+    
     cy.get('[href="/aboutme"]').first().click();
     cy.contains('This is the part where I get to talk about myself!')
   })
-
+  
   it('Visits the contact page', function () {
+    cy.visit('http://localhost:4000')
     cy.get('[href="/contact"]').first().click();
     cy.contains('Guilherme Stecanella')
+
   })
 })
